62a9e7284b73447909739a41164f5ce4c99adc4b
11CaptainKirk <rk@robertjkirk.com>
2021-07-07 16:58:58 -0400

    Added PID functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,109 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <PID_v1.h>
+
+// Setup Servos
+int servoYPin = 4;  // Set Servo Pin
+Servo ServoY;      // Create Servo Object
+int servoZPin = 3;  
+Servo ServoZ;      
+//
+
+// Setup IMU
+Adafruit_BNO055 bno = Adafruit_BNO055(55); // Create IMU Object
+//
+
+// Setup PID Controllers
+double Setpoint;
+double InputY, InputZ;
+double OutputY, OutputZ;
+
+PID PIDy(&InputY, &OutputY, &Setpoint, 2, 5, 1, DIRECT); // PID Y //TODO: Add variables to PID values.
+PID PIDz(&InputZ, &OutputZ, &Setpoint, 2, 5, 1, DIRECT); // PID Z
+//
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // Initialize Servos
+  ServoY.attach(servoYPin);
+  ServoZ.attach(servoZPin);
+  //
+
+  // Initialize Serial Readout
+  Serial.begin(9600);
+  Serial.println("Orientation Sensor Test");
+  Serial.println("");
+  //
+
+  // Initialize IMU
+  if(!bno.begin()){
+    Serial.print("Oops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  delay(1000); //TODO: Why this delay?
+  bno.setExtCrystalUse(true);
+  //
+
+  // Initialize PID
+  InputY = 0;
+  InputZ = 0;
+  Setpoint = 0;
+
+  PIDy.SetMode(AUTOMATIC); // Turn PID on
+  PIDz.SetMode(AUTOMATIC); 
+  //
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+int servoHome = 90; // Set Home Position of servo (degrees)
+
+// Get new sensor event
+sensors_event_t event;
+bno.getEvent(&event);
+//
+
+// Update inputs & compute PID
+InputY = event.orientation.y;
+InputZ = event.orientation.z;
+
+PIDy.Compute();
+PIDz.Compute();
+//
+
+// Move Servos based on output
+if(OutputY > 10){
+  ServoY.write(servoHome+20);
+} else if(OutputY < -10){
+  ServoY.write(servoHome-20);
+} else {
+  ServoY.write(servoHome+(OutputY*2));
+}
+
+if(OutputZ > 10){
+  ServoZ.write(servoHome+20);
+} else if(OutputZ < -10){
+  ServoZ.write(servoHome-20);
+} else {
+  ServoZ.write(servoHome+(OutputZ*2));
+}
+//
+
+
+// Display FP data
+Serial.print("X: ");
+Serial.print(event.orientation.x,4);
+Serial.print("\tY: ");
+Serial.print(event.orientation.y,4);
+Serial.print("\tZ: ");
+Serial.print(event.orientation.z,4);
+Serial.println("");
+
+//  delay(25);
+
+
 }
